8e77d8867f58f15f4cd9ba0412450bc057c2d1e7
jiazhoufan <1104913744@qq.com>
2016-11-06 22:53:35 +0800

    modification of urls

--- AcaPush/urls.py
+++ AcaPush/urls.py
@@ -1,21 +1,22 @@
-"""AcaPush URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
+from myproject.views import news
+from myproject.views import notice
+from myproject.views import comments
+from myproject.views import search
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+	url(r'^admin/$', admin.site.urls),
+
+	url(r'^news/(\d{2})/(\d{8})/$', news, name='news'), #return the news of different academy before the date(00 or 00000000 will return all)
+
+	url(r'^notice/(\d{2})/(\d{8})/$', notice, name='notice'), #return the notice of different academy before the date(00 or 00000000 will return all)
+	
+	
+	url(r'^comments/(\d{2})/$', comments, name='comments'), #return the news of different academy
+	url(r'^comments/upload/$', comments, name='comments'), #upload the comments to the server
+
+	url(r'^search/(\d{1})/(\w{1,15})/(\d{2})/(\d{8})/$', search_news, name='search'), #search the news or notice(flag==0 or flag==1) with keyword（w{1,15}) (00 or 00000000 will return all academy and date news)
+
 ]
+
