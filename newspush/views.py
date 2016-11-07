d7a99c6f51f7f9b033504a244248e7149a4476d7
jiazhoufan <1104913744@qq.com>
2016-11-07 10:53:47 +0800

    fetch and search views

--- newspush/views.py
+++ newspush/views.py
@@ -11,3 +11,26 @@ def commit_comment(request, news_id):
             newsComment.objects.create(
                 
             )
+
+def fetch_news(request):
+    new_id=request.GET['news_id']
+    response_data=serializers.serialize("json",news.objects.filter(id=news_id)
+    return HttpResponse(json.dumps(response_data),content_type="application/json")
+
+def fetch_notice(request):
+    notice_id=request.GET['notice_id']
+    response_data=serializers.serialize("json",notice.objects.filter(id=notice_id)
+    return HttpResponse(json.dumps(response_data),content_type="application/json")
+
+def search_news(request):
+    aca=request.GET['academy']
+    d=request.GET['date']
+    response_data=serializers.serialize("json",news.objects.filter(academy=aca,date<=d)
+    return HttpResponse(json.dumps(response_data),content_type="application/json")
+
+def search_notice(request):
+    aca=request.GET['academy']
+    d=request.GET['date']
+    response_data=serializers.serialize("json",notice.objects.filter(academy=aca,date<=d)
+    return HttpResponse(json.dumps(response_data),content_type="application/json")
+    
