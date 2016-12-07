db0948cf6306519a16f14755e252a2b7cbf66838
ZhiJing Wu <992785291@qq.com>
2016-12-07 17:10:34 +0800

    get specific news

--- dataUpdate.py
+++ dataUpdate.py
@@ -127,9 +127,9 @@ def init_academy():
 def runjar(input):
 	cmd = "java -jar notice_crawler-assembly-0.1.jar"
 	p = subprocess.Popen(cmd,shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
-	out, err = p.communicate(input.encode('gbk'))
+	out, err = p.communicate(input.encode())
 	# print(out.decode())
-	return out.decode('gbk')
+	return out.decode()
 
 # use listsources commend to update listsources
 def listsources():
@@ -218,5 +218,5 @@ def timer(n):
         time.sleep(n)
 
 if __name__ == '__main__':
-	t = 10*5
-	timer(t)
+	import sys
+	getdata(sys.argv[1])
